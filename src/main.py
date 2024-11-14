5da7d3868fd7f098275e43d1eac83ea854b114f6
Mariya Hendriksen <35101262+mariyahendriksen@users.noreply.github.com>
2024-11-14 14:03:49 +0000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,57 +1,57 @@
+"""This script trains and evaluates the CLIP model."""
 import argparse
+import os
 import yaml
 import torch
-import os
 import logging as log
 import pandas as pd
+import itertools
+from typing import Dict, Any
+
 from dataset import ImageCaptionDataset, build_loader
 from model import CLIPModel
-import itertools
 from train_valid import train_epoch, valid_epoch
 from utils import save_pkl, get_dt_string, load_pkl
 
+def main(args: argparse.Namespace) -> None:
+    """
+    Main function to train and evaluate the CLIP model.
 
-def main(args):
+    Args:
+        args (argparse.Namespace): Command line arguments.
+    """
     dataset = args.dataset
     train_config_file = args.config_file
     is_tiny = args.is_tiny
     print(args)
 
-    # logging
     log.basicConfig(
         filename=f'logs/main_{dataset}.log',
         level=log.INFO,
         format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S'
-        )
+    )
 
-    ############################
-    # load: config, image and text dicts, df_train, df_dev
-    ############################
     with open(train_config_file) as file:
         config_train = yaml.safe_load(file)
     print(config_train)
-    log.info(f'Config train:{config_train}')
+    log.info(f'Config train: {config_train}')
     
     torch.manual_seed(config_train["manual_seed"])
-    config_train["device"] = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    config_train["device"] = torch.device(
+        'cuda' if torch.cuda.is_available() else 'cpu'
+    )
 
-    # define what to do if the dataset is tiny
     if is_tiny:
-        # csv_file = config['csv_file_small']
         df_train_file = 'small_df_train.csv'
         df_test_file = 'small_df_test.csv'
         df_dev_file = 'small_df_dev.csv'
         config_train['epochs'] = 1
     else:
-        # csv_file = config['csv_file']
         df_train_file = 'df_train.csv'
         df_test_file = 'df_test.csv'
         df_dev_file = 'df_dev.csv'
 
-
-    # dataset_config_file = '/Users/mhendriksen/Desktop/repositories/CLIP/conf/local_cub_conf.yaml'
-    # text_dict_path = '/Users/mhendriksen/Desktop/repositories/datasets/CUB_200_2011/text_dict.pkl'
     text_dict_path = os.path.join(
         config_train['DATASET'][dataset]['root'],
         config_train['DATASET'][dataset]['text_dict'],
@@ -75,18 +75,10 @@ def main(args):
     df_train = pd.read_csv(df_train_path, index_col=0)
     df_dev = pd.read_csv(df_dev_path, index_col=0)
 
-    ############################
-    # set experimental data
-    ############################
-    # date_string = get_dt_string()
     experiment_id = f'{dataset}_{config_train["epochs"]}E_{config_train["batch_size"]}BS'
     best_model_path = f'{config_train["deliverables_folder"]}/models/best_model_{experiment_id}.pt'
     loss_data_path = f'{config_train["deliverables_folder"]}/meta_data/loss_info_{experiment_id}'
 
-
-    ############################
-    # set: train and valid loaders, model
-    ############################
     train_loader = build_loader(
         dataset=ImageCaptionDataset,
         config=config_train,
@@ -94,7 +86,7 @@ def main(args):
         text_dict=text_dict,
         image_dict=image_dict,
         mode='train'
-        )
+    )
     valid_loader = build_loader(
         dataset=ImageCaptionDataset,
         config=config_train,
@@ -102,14 +94,12 @@ def main(args):
         text_dict=text_dict,
         image_dict=image_dict,
         mode='test'
-        )
+    )
     print('Built loaders!')
     log.info('Built loaders!')
 
-
     model = CLIPModel(config=config_train).to(config_train["device"])
 
-    # get optimizer
     params = [
         {
             'params': itertools.chain(
@@ -131,9 +121,6 @@ def main(args):
     print('Created CLIP model')
     log.info('Created CLIP model')
 
-    ############################
-    # training and evaluation
-    ############################
     step = "epoch"
     train_loss_values = []
     valid_loss_values = []
@@ -143,7 +130,6 @@ def main(args):
         print(f"Epoch: {epoch + 1}")
         model.train()
         train_loss = train_epoch(config_train, model, train_loader, optimizer, lr_scheduler, step)
-        # print(train_loss.avg)
         train_loss_values.append(train_loss.avg)
         
         model.eval()
@@ -153,19 +139,15 @@ def main(args):
 
         if valid_loss.avg < best_loss:
             best_loss = valid_loss.avg
-
-            # dt_string = get_dt_string()
             torch.save(model.state_dict(), best_model_path)
             print(f"Saved Best Model to {best_model_path}! best loss - {best_loss}")
             log.info(f"Saved Best Model! best loss - {best_loss}")
-            # torch.save(valid_loader, f"{CFG.deliverables_path}/{dt_string}_valid_loader.pth")
-            # print('Saved the dataloader!')
 
         lr_scheduler.step(valid_loss.avg)
     print('Finished training!')
     log.info('Finished training!')
 
-    # build loss dict, save loss values
+    # Build loss dict, save loss values
     loss_values = {
         'train_loss': train_loss_values,
         'valid_loss': valid_loss_values
@@ -176,13 +158,27 @@ def main(args):
     log.info('Finished running the script!')
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', type=str,
-                        default='CUB',
-                        choices=['CUB', 'ABO', 'fashion200k', 'coco', 'f30k'],
-                        help='dataset type')
-    parser.add_argument('--config_file', type=str, default='conf/train_conf.yaml',
-                    help='Configuration file')
-    parser.add_argument('--is_tiny', type=bool, default=False)
+    parser = argparse.ArgumentParser(
+        description="Script to train and evaluate the CLIP model."
+    )
+    parser.add_argument(
+        '--dataset',
+        type=str,
+        default='CUB',
+        choices=['CUB', 'ABO', 'fashion200k', 'coco', 'f30k'],
+        help='Dataset type'
+    )
+    parser.add_argument(
+        '--config_file',
+        type=str,
+        default='conf/train_conf.yaml',
+        help='Configuration file'
+    )
+    parser.add_argument(
+        '--is_tiny',
+        type=bool,
+        default=False,
+        help='Use tiny version of the dataset'
+    )
     args = parser.parse_args()
     main(args)
